b4dc3f0e45bf26e6766c72694e19eb6e4d6e9e9b
mini-u <sungminryu127@gmail.com>
2025-05-22 20:02:51 +0900

    0522SM_add_prototype

---
+++ dashboard/prototype_v1.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+# 타이틀틀
+st.title("프로토타입 레이아웃")
+
+# 분할
+left_col, right_col = st.columns([2, 3])
+
+# 지도
+with left_col:
+    st.subheader("지도")
+    st.markdown("경남지도표시")
+
+# 데이터
+with right_col:
+    st.subheader("공급")
+    st.markdown("문화시설, 접근성")
+
+    st.divider()
+
+    st.subheader("수요")
+    st.markdown("사람, 생활인구, 거주인구 등")
+
+    st.divider()
+
+    st.subheader("전략")
+    st.markdown("무슨전략을 제시하지지")
