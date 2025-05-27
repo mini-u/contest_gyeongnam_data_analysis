8e86c3c3f0824b75647b5c741cca8bb9f24a8d6d
mini-u <sungminryu127@gmail.com>
2025-05-27 10:38:55 +0900

    0527SM add map

--- dashboard/prototype_v1.py
+++ dashboard/prototype_v1.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import geopandas as gpd
+import folium
+from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide")
 
@@ -12,7 +15,27 @@ left_col, right_col = st.columns([2, 3])
 with left_col:
     st.subheader("지도")
     st.markdown("경남지도표시")
+    
+    gdf = gpd.read_file(r"D:\MINNIE\contest_gyeongnam_data_analysis\data_for_dashboard\GN_map.geojson")
+    
+    center = gdf.geometry.union_all().centroid
+    m = folium.Map(location=[center.y, center.x], zoom_start=8.5, tiles=None)
+    
+    # GeoJson 추가
+    geojson_layer = folium.GeoJson(
+        gdf,
+        tooltip=folium.GeoJsonTooltip(fields=["SGG_NM_cleaned"]),
+        style_function=lambda x: {
+            "fillColor": "white",
+            "color": "black",
+            "weight": 1,
+            "fillOpacity": 0.0
+        },
+    )
+    geojson_layer.add_to(m)
 
+    clicked_area = st_folium(m, width=850, height=650)
+    
 # 데이터
 with right_col:
     st.subheader("공급")
